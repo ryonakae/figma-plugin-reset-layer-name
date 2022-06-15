3ab3cb6b6b90b16a393183247e06c2feb88a449a
Ryo Nakae <ryo.n.1129@gmail.com>
2022-06-15 23:22:03 +0900

    add

---
+++ src/code.ts
@@ -0,0 +1,120 @@
+import { findIndex } from 'lodash'
+
+// find系の高速化
+// figma.skipInvisibleInstanceChildren = true
+
+// 右パネルに起動ボタンを表示
+// figma.root.setRelaunchData({ open: '' })
+
+if (figma.currentPage.selection.length) {
+  // 処理後に選択を更新するので、それ用の配列を用意
+  const newSelection: SceneNode[] = []
+
+  // 選択している要素ごとに処理を実行
+  figma.currentPage.selection.forEach(node => {
+    console.log(node)
+
+    // 親のnodeを取得
+    const parentNode = node.parent
+
+    // parentNodeが無い場合は処理中断
+    if (!parentNode) {
+      figma.notify('Parent element not found.')
+      return
+    }
+
+    // parentNodeがインスタンスの場合
+    if (parentNode.type === 'INSTANCE') {
+      // インスタンスのmainComponentを取得
+      const mainComponent = parentNode.mainComponent
+
+      // mainComponentが無い場合は処理中断
+      if (!mainComponent) {
+        figma.notify('Main component not found.')
+        return
+      }
+
+      // インスタンスのchildrenでのnodeのインデックスを取得
+      const index = findIndex(parentNode.children, child => {
+        return child.id === node.id
+      })
+
+      // nodeの名前をmainComponentにある要素と同じ名前にする
+      node.name = mainComponent.children[index].name
+
+      // newSelectionにnodeを追加
+      newSelection.push(node)
+    }
+    // nodeがテキストの場合
+    else if (node.type === 'TEXT') {
+      // autoRenameをtrueにするだけで名前がリセットされる
+      node.autoRename = true
+
+      // newSelectionにnodeを追加
+      newSelection.push(node)
+    }
+    // nodeがそれ以外の場合
+    else {
+      // nodeをcloneする
+      const clonedNode = node.clone()
+
+      // nodeがインスタンスの場合→mainComponentの名前にする
+      if (node.type === 'INSTANCE') {
+        // mainComponentを取得
+        const mainComponent = node.mainComponent
+
+        // mainComponentが無い場合は処理中断
+        if (!mainComponent) {
+          figma.notify('Main component not found.')
+          return
+        }
+
+        // mainComponentのさらに親を取得
+        const parentMainComponent = mainComponent.parent
+
+        // parentMainComponentがCOMPONENT_SET、つまりVariantsの場合
+        if (
+          parentMainComponent &&
+          parentMainComponent.type === 'COMPONENT_SET'
+        ) {
+          // clonedNodeの名前をparentMainComponentのものにする
+          clonedNode.name = parentMainComponent.name
+        }
+        // それ以外（ただのコンポーネント）の場合
+        else {
+          // clonedNodeの名前をmainComponentのものにする
+          clonedNode.name = mainComponent.name
+        }
+      }
+      // それ以外の場合
+      else {
+        // clonedNodeの名前を空にする→デフォルトの名前になる
+        clonedNode.name = ''
+      }
+
+      // nodeのインデックスを取得
+      const index = findIndex(parentNode.children, child => {
+        return child.id === node.id
+      })
+
+      // インデックスを元に、nodeの後にclonedNodeを移動
+      parentNode.insertChild(index + 1, clonedNode)
+
+      // newSelectionにclonedNodeを追加
+      newSelection.push(clonedNode)
+
+      // nodeを削除
+      node.remove()
+    }
+
+    // 現在のselectionをnewSelectionsにする
+    figma.currentPage.selection = newSelection
+
+    figma.notify('Reset selected layers name!')
+  })
+} else {
+  figma.notify('Please select at least one layer.')
+}
+
+// プラグインを終了
+figma.closePlugin()
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = (env, argv) =>
+  /** @type {import('webpack').Configuration} */
+  ({
+    mode: argv.mode === 'production' ? 'production' : 'development',
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
+    entry: {
+      code: './src/code.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts'],
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            sourceMap: false,
+            warnings: false,
+            compress: {
+              pure_funcs: ['console.log', 'console.error', 'console.warn']
+            },
+            output: {
+              comments: /@license/i
+            }
+          }
+        })
+      ]
+    }
+  })
