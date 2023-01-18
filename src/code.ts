55dfbae15a814e09c550afc0fa8ad3c9284e2e68
Ryo Nakae <ryo.n.1129@gmail.com>
2023-01-18 20:06:16 +0900

    update

--- src/code.ts
+++ src/code.ts
@@ -1,93 +1,89 @@
-import { findIndex } from 'lodash'
+import {
+  getAncestorInstances,
+  getIndexStructureInInstance,
+  getNodeInComponentByIndexStructure
+} from '@/util'
 
 // find系の高速化
 // figma.skipInvisibleInstanceChildren = true
 
-// nodeの先祖が特定のタイプかどうかを返す再帰関数
-// page or documentまで遡り、先祖node or undefinedを返す
-function getAncestorNodeByType(
-  node: SceneNode,
-  type: SceneNode['type']
-): SceneNode | undefined {
-  if (!node.parent) {
-    return undefined
-  } else if (node.parent.type === type) {
-    return node.parent
-  } else if (node.parent.type === 'PAGE' || node.parent.type === 'DOCUMENT') {
-    return undefined
-  } else {
-    return getAncestorNodeByType(node.parent, type)
-  }
+// エラーメッセージ集
+const errorMessage = {
+  common: 'Something went wrong.',
+  alreadyReset: 'Layer names have already been reset.'
 }
 
 if (figma.currentPage.selection.length) {
   // 選択している要素ごとに処理を実行
-  figma.currentPage.selection.forEach(node => {
+  figma.currentPage.selection.forEach(async node => {
     console.log(node)
 
-    // nodeの先祖がComponentの場合
-    if (getAncestorNodeByType(node, 'COMPONENT')) {
-      console.log(node.id, getAncestorNodeByType(node, 'COMPONENT')?.id)
-    }
-    // nodeの先祖がインスタンスの場合
-    // 無理やり元の名前にリネームしているだけで、overrideをリセットしているわけではない
-    // (reset overrideできるAPIが現時点では無い)
-    else if (getAncestorNodeByType(node, 'INSTANCE')) {
-      // 親nodeを取得
-      const parentNode = node.parent as InstanceNode
-
-      // インスタンスのmainComponentを取得
-      const mainComponent = parentNode.mainComponent
-
-      // mainComponentが無い場合は処理中断
-      if (!mainComponent) {
-        figma.notify('Main component not found.')
-        return
-      }
-
-      // インスタンスのchildrenでのnodeのインデックスを取得
-      const index = findIndex(parentNode.children, child => {
-        return child.id === node.id
-      })
-
-      // nodeの名前をmainComponentにある要素と同じ名前にする
-      node.name = mainComponent.children[index].name
-    }
-    // nodeがインスタンスの場合
-    else if (node.type === 'INSTANCE') {
-      // インスタンスのmainComponentを取得
-      const mainComponent = node.mainComponent
-
-      // mainComponentが無い場合は処理中断
-      if (!mainComponent) {
-        figma.notify('Main component not found.')
-        return
-      }
-
-      // mainComponentの親を取得
-      const parentMainComponent = mainComponent.parent as SceneNode
-
-      // mainComponentの親がVariantsの場合
-      if (parentMainComponent.type === 'COMPONENT_SET') {
-        // nodeの名前をVariantsの名前にする
-        node.name = parentMainComponent.name
-      }
-      // それ以外の場合（普通のコンポーネントの場合）
-      else {
-        // nodeの名前をmainComponentの名前にする
-        node.name = mainComponent.name
-      }
-    }
     // nodeがコンポーネント or Variantsの場合
-    else if (node.type === 'COMPONENT' || node.type === 'COMPONENT_SET') {
+    if (node.type === 'COMPONENT' || node.type === 'COMPONENT_SET') {
       // 名前をデフォルトに戻されると困るので、処理中断
       figma.notify('This element is component or variants.')
       return
     }
+
     // nodeがそれ以外の場合
     else {
-      // 名前を空にするとリセットされる
-      node.name = ''
+      // 先祖インスタンスを取得
+      const ancestorInstances = getAncestorInstances(node)
+      console.log('ancestorInstances', ancestorInstances)
+
+      // 先祖インスタンスがある場合（nodeはインスタンスの子要素）
+      if (ancestorInstances.length > 0) {
+        console.log('ancestorInstances exist')
+
+        // 先祖インスタンスのメインコンポーネントを取得する
+        const mainComponentOfRootAncestorInstance =
+          ancestorInstances[0].mainComponent
+        console.log(
+          'mainComponentOfRootAncestorInstance',
+          mainComponentOfRootAncestorInstance,
+          mainComponentOfRootAncestorInstance?.name,
+          mainComponentOfRootAncestorInstance?.remote
+        )
+
+        // メインコンポーネントが無い場合は処理中断
+        if (!mainComponentOfRootAncestorInstance) {
+          return figma.notify(errorMessage.common)
+        }
+
+        // nodeのインデックス構造を取得する
+        const indexStructure = getIndexStructureInInstance(node)
+        console.log('indexStructure', indexStructure)
+
+        // 取得したインデックス構造から、先祖インスタンスのメインコンポーネント内の、
+        // nodeと同じ要素を取得する
+        const sameNode = getNodeInComponentByIndexStructure(
+          mainComponentOfRootAncestorInstance,
+          indexStructure
+        )
+        console.log('sameNode', sameNode)
+
+        // 同じ要素が無い場合は処理中断
+        if (!sameNode) {
+          return figma.notify(errorMessage.common)
+        }
+
+        // nodeの名前がsameNodeの名前とすでに同じ場合
+        if (node.name === sameNode.name) {
+          return figma.notify(errorMessage.alreadyReset)
+        }
+        // 違う場合→リネーム
+        else {
+          node.name = sameNode.name
+        }
+      }
+
+      // 先祖インスタンスがない場合
+      else {
+        console.log('no ancestorInstances')
+
+        // 名前を空にする（リセットされる）
+        node.name = ''
+      }
     }
 
     figma.notify('Reset selected layers name!')
